9b6eb98765de71fcc75de5d8562686133ec40563
Ben Basuni <benbasuni1@gmail.com>
2018-01-09 02:18:58 -0800

    tried editing with linter, ended up using beautify plugin for vscode

--- xelp-app/server/index.js
+++ xelp-app/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('../database');
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
