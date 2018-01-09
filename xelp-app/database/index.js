9b7cd7dfe7721a35f76ee55552c24fd2225a6c83
Ben Basuni <benbasuni1@gmail.com>
2018-01-09 01:35:46 -0800

    updated readme and added dependencies, webpack setup and nodemon

--- xelp-app/database/index.js
+++ xelp-app/database/index.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+//const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'movielist'
-});
+//var connection = mysql.createConnection({
+    //host     : 'localhost',
+    //user     : 'root',
+    //password : '',
+    //database : 'movielist'
+//});
 
-module.exports = {};
+//module.exports = {};
--- xelp-app/server/index.js
+++ xelp-app/server/index.js
@@ -6,6 +6,3 @@ const db = require('../database');
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.listen(3000);
-
-app.get('/item1', (req, res) => {});
-app.post('/item1', (req, res) => {});
