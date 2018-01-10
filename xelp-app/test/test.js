910d1d3c5cfbb40136e6707e0c5ff4a336a2382c
TomHumphrey150 <TomHumphrey@Toms-MacBook-Air-8.local>
2018-01-10 11:12:12 -0800

    added first real server test

--- xelp-app/test/test.js
+++ xelp-app/test/test.js
@@ -1,4 +1,6 @@
-let assert = require('assert');
+const assert = require('assert');
+const axios = require('axios');
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -7,6 +9,18 @@ describe('Array', function() {
   });
 });
 
-var newVar = 0;
+const baseUrl = `http://localhost:${process.env.PORT}`;
 
-// change
+describe('GET /', () => {
+  it('responds with a status code of 200', (done) => {
+    axios.get(`${baseUrl}/`)
+      .then((response) => {
+        assert.equal(response.status, 200);
+        // expect(response.status).to.equal(200);
+        done();
+      })
+      .catch((err) => {
+        console.log('There was an error requesting / from the server', err);
+      });
+  });
+});
