adc26141b66305f666b0f141a21acc72fe6d46b3
tylercable1 <tylercable7@gmail.com>
2018-01-11 12:23:43 -0800

    changed icon names for readability

--- xelp-app/client/src/components/SingleRestaurant.jsx
+++ xelp-app/client/src/components/SingleRestaurant.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
-import LocationOn from 'material-ui/svg-icons/communication/location-on';
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-import Phone from 'material-ui/svg-icons/communication/phone';
-import Money from 'material-ui/svg-icons/editor/attach-money';
-import AddPhoto from 'material-ui/svg-icons/image/add-a-photo';
-import WriteReview from 'material-ui/svg-icons/editor/mode-edit';
-import CellPhone from 'material-ui/svg-icons/hardware/phone-iphone';
-import Car from 'material-ui/svg-icons/maps/directions-car';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import LocationOnIcon from 'material-ui/svg-icons/communication/location-on';
+import ActionGradeIcon from 'material-ui/svg-icons/action/grade';
+import PhoneIcon from 'material-ui/svg-icons/communication/phone';
+import MoneyIcon from 'material-ui/svg-icons/editor/attach-money';
+import AddPhotoIcon from 'material-ui/svg-icons/image/add-a-photo';
+import WriteReviewIcon from 'material-ui/svg-icons/editor/mode-edit';
+import CellPhoneIcon from 'material-ui/svg-icons/hardware/phone-iphone';
+import CarIcon from 'material-ui/svg-icons/maps/directions-car';
 
 class SingleRestaurant extends React.Component {
   constructor(props) {
@@ -20,14 +21,14 @@ class SingleRestaurant extends React.Component {
         <h1>{this.props.restaurant.name}</h1>
         <div>
           <List>
-            <ListItem primaryText={`${this.props.restaurant.location.address1}, ${this.props.restaurant.location.city}, ${this.props.restaurant.location.state}, ${this.props.restaurant.location.zip_code}`} leftIcon={<LocationOn />} />
-            <ListItem primaryText={this.props.restaurant.rating} leftIcon={<ActionGrade />} />
-            <ListItem primaryText={this.props.restaurant.display_phone || 'None listed'} leftIcon={<Phone />} />
-            <ListItem primaryText={this.props.restaurant.price} leftIcon={<Money />} />
-            <ListItem primaryText="Send via SMS" leftIcon={<CellPhone />} />
-            <ListItem primaryText="Upload Photo" leftIcon={<AddPhoto />} />
-            <ListItem primaryText="Get Directions" leftIcon={<Car />} />
-            <Link to="restaurant/writeReview"> <ListItem primaryText="Write Review" leftIcon={<WriteReview />} /> </Link>
+            <ListItem primaryText={`${this.props.restaurant.location.address1}, ${this.props.restaurant.location.city}, ${this.props.restaurant.location.state}, ${this.props.restaurant.location.zip_code}`} leftIcon={<LocationOnIcon />} />
+            <ListItem primaryText={this.props.restaurant.rating} leftIcon={<ActionGradeIcon />} />
+            <ListItem primaryText={this.props.restaurant.display_phone || 'None listed'} leftIcon={<PhoneIcon />} />
+            <ListItem primaryText={this.props.restaurant.price} leftIcon={<MoneyIcon />} />
+            <ListItem primaryText="Send via SMS" leftIcon={<CellPhoneIcon />} />
+            <ListItem primaryText="Upload Photo" leftIcon={<AddPhotoIcon />} />
+            <ListItem primaryText="Get Directions" leftIcon={<CarIcon />} />
+            <Link to="restaurant/writeReview"> <ListItem primaryText="Write Review" leftIcon={<WriteReviewIcon />} /> </Link>
           </List>
         </div>
       </div>
