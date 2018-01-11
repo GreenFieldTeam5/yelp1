9a7a6204d3561f35eb9a42daf1e547e5ca98ae5d
tylercable1 <tylercable7@gmail.com>
2018-01-10 17:01:46 -0800

    Started working on restaurant page.

--- xelp-app/client/src/components/SingleRestaurant.jsx
+++ xelp-app/client/src/components/SingleRestaurant.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import {List, ListItem} from 'material-ui/List';
+import LocationOn from 'material-ui/svg-icons/communication/location-on';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
 
 class SingleRestaurant extends React.Component {
   constructor(props) {
@@ -7,7 +14,16 @@ class SingleRestaurant extends React.Component {
   render() {
     return (
       <div>
-        RESTAURANT PAGE
+        <h1>{this.props.restaurant.name}</h1>
+        <div>
+          <List>
+            <ListItem primaryText={`${this.props.restaurant.location.address1}, ${this.props.restaurant.location.city}, ${this.props.restaurant.location.state}, ${this.props.restaurant.location.zip_code}`} leftIcon={<LocationOn />} />
+            <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+            <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
+            <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
+            <ListItem primaryText="Inbox" leftIcon={<LocationOn />} />
+          </List>
+        </div>
       </div>
     );
   }
