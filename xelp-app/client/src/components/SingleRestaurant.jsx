1b4b95afcf1ea69ef092eca30914ca1ffc8a6432
tylercable1 <tylercable7@gmail.com>
2018-01-10 19:55:24 -0800

    Restaraunt page list complete. Fixed images on home page.

--- xelp-app/client/src/components/SingleRestaurant.jsx
+++ xelp-app/client/src/components/SingleRestaurant.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {List, ListItem} from 'material-ui/List';
 import LocationOn from 'material-ui/svg-icons/communication/location-on';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
-import ContentSend from 'material-ui/svg-icons/content/send';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
-import Divider from 'material-ui/Divider';
-import ActionInfo from 'material-ui/svg-icons/action/info';
+import Phone from 'material-ui/svg-icons/communication/phone';
+import Money from 'material-ui/svg-icons/editor/attach-money';
+import AddPhoto from 'material-ui/svg-icons/image/add-a-photo';
+import WriteReview from 'material-ui/svg-icons/editor/mode-edit';
+import CellPhone from 'material-ui/svg-icons/hardware/phone-iphone';
+import Car from 'material-ui/svg-icons/maps/directions-car';
 
 class SingleRestaurant extends React.Component {
   constructor(props) {
@@ -18,10 +20,12 @@ class SingleRestaurant extends React.Component {
         <div>
           <List>
             <ListItem primaryText={`${this.props.restaurant.location.address1}, ${this.props.restaurant.location.city}, ${this.props.restaurant.location.state}, ${this.props.restaurant.location.zip_code}`} leftIcon={<LocationOn />} />
-            <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
-            <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
-            <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
-            <ListItem primaryText="Inbox" leftIcon={<LocationOn />} />
+            <ListItem primaryText={this.props.restaurant.rating} leftIcon={<ActionGrade />} />
+            <ListItem primaryText={this.props.restaurant.display_phone || 'None listed'} leftIcon={<Phone />} />
+            <ListItem primaryText={this.props.restaurant.price} leftIcon={<Money />} />
+            <ListItem primaryText="Write Review" leftIcon={<WriteReview />} />
+            <ListItem primaryText="Upload Photo" leftIcon={<AddPhoto />} />
+            <ListItem primaryText="Get Directions" leftIcon={<Car />} />
           </List>
         </div>
       </div>
