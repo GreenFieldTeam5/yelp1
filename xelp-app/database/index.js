08ca5bd37f27293644d8a03c29c4e7821d558d0f
Ben Basuni <benbasuni1@gmail.com>
2018-01-12 12:05:56 -0800

    working on setting up heroku and postgres db

--- xelp-app/database/index.js
+++ xelp-app/database/index.js
@@ -1,4 +1,20 @@
 const knex = require('./db');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+console.log('connect successful!');
+// client.query('SELECT * FROM restaurants;', (err, res) => {
+// if (err) throw err;
+// for (const row of res.rows) {
+// console.log(JSON.stringify(row));
+// }
+// client.end();
+// });
 
 const facebookLogin = (profile) => {
   knex.insert({
