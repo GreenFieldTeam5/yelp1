9a86ab95a99f832fd7f1c8b03daba6badde551b4
TomHumphrey150 <TomHumphrey@Toms-MacBook-Air-8.local>
2018-01-12 11:50:22 -0800

    Added annotations to the map

--- xelp-app/client/src/components/Map.jsx
+++ xelp-app/client/src/components/Map.jsx
@@ -7,6 +7,7 @@ class Map extends React.Component {
 
   constructor(props) {
     super(props);
+    console.log()
     this.state = {
       longitude: 5,
       latitude: 34,
@@ -24,14 +25,53 @@ class Map extends React.Component {
       zoom
     });
 
+    map.on('load', function () {
+      map.addLayer({
+        "id": "points",
+        "type": "symbol",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [-77.03238901390978, 38.913188059745586]
+                    },
+                    "properties": {
+                        "title": "Mapbox DC",
+                        "icon": "monument"
+                    }
+                }, {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [-122.414, 37.776]
+                    },
+                    "properties": {
+                        "title": "Mapbox SF",
+                        "icon": "harbor"
+                    }
+                }]
+            }
+        },
+        "layout": {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+        },
+      });
+    });
+
     map.on('move', () => {
-
       const { lng, lat } = map.getCenter();
-
       this.setState({
         longitude: lng.toFixed(4), // toFixed(4) means to 4 decimal accuracy
         latitude: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+        zoom: map.getZoom().toFixed(2),
       });
     });
   }
