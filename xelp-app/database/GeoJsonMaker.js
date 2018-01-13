e9eae5b0ec409bb4a348f6408fcf299d38490aa6
TomHumphrey150 <TomHumphrey@toms-air-8.attlocal.net>
2018-01-13 15:38:52 -0800

    Map fixed for knex

--- xelp-app/database/GeoJsonMaker.js
+++ xelp-app/database/GeoJsonMaker.js
@@ -1,23 +1,30 @@
-const convertRestaurantsToGeoJSON = (jsonRestaurants) => {
+const convertRestaurantsToGeoJSON = (restaurants, isYelpAPI) => {
+
+  console.log('restaurants convertRestaurantsToGeoJSON:', restaurants);
+
   const geoJSON = {};
   geoJSON.type = 'FeatureCollection';
   geoJSON.features = [];
-  for (let i = 0; i < jsonRestaurants.length; i += 1) {
-    const jsonRestaurant = jsonRestaurants[i];
+  for (let i = 0; i < restaurants.length; i += 1) {
+    const restaurant = restaurants[i];
     const newGeoRestObj = {};
     newGeoRestObj.type = 'Feature';
 
     const propertiesSubObject = {};
-    propertiesSubObject.title = jsonRestaurant.name;
+    propertiesSubObject.title = restaurant.name;
     propertiesSubObject.icon = 'monument';
     newGeoRestObj.properties = propertiesSubObject;
 
     const geometrySubObject = {};
     geometrySubObject.type = 'Point';
-    const coords = jsonRestaurant.coordinates;
-    geometrySubObject.coordinates = [coords.longitude, coords.latitude];
-    newGeoRestObj.geometry = geometrySubObject;
-
+    if (isYelpAPI) {
+      const coords = restaurant.coordinates;
+      geometrySubObject.coordinates = [coords.longitude, coords.latitude];
+      newGeoRestObj.geometry = geometrySubObject;
+    } else {
+      geometrySubObject.coordinates = [restaurant.longitude, restaurant.latitude];
+      newGeoRestObj.geometry = geometrySubObject;
+    }
     geoJSON.features.push(newGeoRestObj);
   }
   return geoJSON;
