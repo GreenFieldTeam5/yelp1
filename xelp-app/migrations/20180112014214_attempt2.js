ad48471974c76f4b7e2f519558f9915693567576
Ben Basuni <benbasuni1@gmail.com>
2018-01-13 11:53:49 -0800

    modified migrations to mirror database

--- xelp-app/migrations/20180112014214_attempt2.js
+++ xelp-app/migrations/20180112014214_attempt2.js
@@ -4,24 +4,16 @@ exports.up = (knex, Promise) => Promise.all([
     users.increments('uid').notNullable().primary();
     users.biginteger('facebook_id').unique();
     users.string('username').notNullable();
-    users.string('first_name');
-    users.string('last_name');
-    users.string('phone_number');
-    users.string('email');
-    users.timestamps(true, true);
+    users.timestamp('created_at').defaultTo(knex.fn.now());
   }),
   knex.schema.createTableIfNotExists('reviews', (reviews) => {
     reviews.increments('id').primary();
-    reviews.string('review_text');
-    reviews.float('avg_rating');
-    reviews.string('image_url');
     reviews.integer('user_id').references('uid').inTable('users').onDelete('CASCADE');
-    reviews.timestamps(true);
-  }),
-  knex.schema.createTableIfNotExists('restaurant_reviews', (restaurantReviews) => {
-    restaurantReviews.increments('id').notNullable().primary();
-    restaurantReviews.integer('review_id').references('id').inTable('reviews').onDelete('CASCADE');
-    restaurantReviews.integer('restaurant_id').references('id').inTable('restaurants').onDelete('CASCADE');
+    reviews.integer('restaurant_id').references('id').inTable('restaurants').onDelete('CASCADE');
+    reviews.float('avg_rating');
+    reviews.string('review_text');
+    reviews.string('image_url');
+    reviews.timestamp('created_at').defaultTo(knex.fn.now());
   }),
   knex.schema.createTableIfNotExists('restaurants', (restaurants) => {
     restaurants.increments('id').notNullable().primary();
@@ -33,10 +25,10 @@ exports.up = (knex, Promise) => Promise.all([
     restaurants.string('state');
     restaurants.string('zip_code');
     restaurants.string('price');
-    restaurants.float('rating');
     restaurants.string('categories');
-    restaurants.float('latitude');
+    restaurants.float('rating');
     restaurants.float('longitude');
+    restaurants.float('latitude');
   }),
   knex.schema.createTableIfNotExists('users_restaurants_recently_viewed', (userRestaurants) => {
     userRestaurants.increments('id').notNullable().primary();
@@ -50,11 +42,8 @@ exports.down = (knex, Promise) => Promise.all([
   knex.schema.table('reviews', (reviews) => {
     reviews.dropForeign('user_id');
   }),
-  knex.schema.table('restaurant_reviews', (restaurantReviews) => {
-    restaurantReviews.dropForeign('review_id');
-  }),
-  knex.schema.table('restaurant_reviews', (restaurantReviews) => {
-    restaurantReviews.dropForeign('restaurant_id');
+  knex.schema.table('reviews', (reviews) => {
+    reviews.dropForeign('restaurant_id');
   }),
   knex.schema.table('users_restaurants_recently_viewed', (userRestaurants) => {
     userRestaurants.dropForeign('restaurant_id');
@@ -64,7 +53,6 @@ exports.down = (knex, Promise) => Promise.all([
   }),
   knex.schema.dropTableIfExists('users'),
   knex.schema.dropTableIfExists('reviews'),
-  knex.schema.dropTableIfExists('restaurant_reviews'),
   knex.schema.dropTableIfExists('restaurants'),
   knex.schema.dropTableIfExists('users_restaurants_recently_viewed'),
 ]);
